a9e3bb218d1e220fd6e0dcac913a5435184678cf
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-14 09:55:52 -0700

    Still broken, I am on the verge of fixing the if else statements and the priority

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,28 @@
 // 3.Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."
 
 var convertedNumbers;
-
+var stringArray = [];
 
  $(function() {
    $("form").click(function(event) {
    event.preventDefault();
    string = ($("#input1").val());
    console.log(string);
+   stringArray = string.split("");
 
-   for (var i = 0; i < string.length; i ++) {
-    if (string[i].includes("3")) {
-      string[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
-      console.log(string);
-    } else if (string[i].includes("2") && !string.includes("3")) {
-      string[i] = "Boop!";
-    } else if (string[i].includes("1") && !string.includes("2") && !string.includes("3")) {
-      string[i] = "Beep!";
+   for (var i = 0; i < stringArray.length -1; i ++) {
+    if (stringArray[i].includes("3")) {
+      stringArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+
+    } else if (stringArray[i].includes("2") && !stringArray.includes("3")) {
+      stringArray[i] = "Boop!";
+
+    } else if (stringArray[i].includes("1") && !stringArray.includes("2") && !stringArray.includes("3")) {
+      stringArray[i] = "Beep!";
+
    }
  }
+   console.log(stringArray);
  });
 
 
