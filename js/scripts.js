2ec8780808216494535e1f9034f3a02d99b1d1db
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-14 09:34:22 -0700

    Add prioritization but broke everything, woo hoo :)

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,24 @@
 // 3.Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."
 
 var convertedNumbers;
-var stringArray = [];
+
 
  $(function() {
    $("form").click(function(event) {
    event.preventDefault();
    string = ($("#input1").val());
    console.log(string);
-   var stringArray = string.split("");
-   for (var i = 0; i < stringArray.length; i ++) {
-    if (stringArray[i].includes("3")) {
-      stringArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
-      console.log(stringArray);
-    } else if (stringArray[i].includes("2")) {
-      stringArray[i] = "Boop!";
-    }
-      else if (stringArray[i].includes("1")) {
-      stringArray[i] = "Beep!";
+
+   for (var i = 0; i < string.length; i ++) {
+    if (string[i].includes("3")) {
+      string[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+      console.log(string);
+    } else if (string[i].includes("2") && !string.includes("3")) {
+      string[i] = "Boop!";
+    } else if (string[i].includes("1") && !string.includes("2") && !string.includes("3")) {
+      string[i] = "Beep!";
    }
+ }
  });
 
 
