c081a2782eedf5311ce193abf8d64ad0a441c31e
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-14 08:53:17 -0700

    Add more work to prepare for scipting functionality, (for loop, and begining of if statment

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// 1.Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+// 2.Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+// 3.Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."
+
+var convertedNumbers;
+
+ $(function() {
+   $("form").click(function(event) {
+   event.preventDefault();
+   string = ($("#input1").val());
+   console.log(string);
+   for (var i = 0; i < #input.length; i ++)
+   if (string.includes 1) {
+
+   }
+ });
+
+
+ });
