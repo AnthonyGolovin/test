f6ed69ad332b6ccbab6c83b9e0212594f13b2791
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-14 09:13:33 -0700

    Create 3 sections to if and else if statements, preparing to find a way to prioritize each if statment in their respective values

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,23 @@
 // 3.Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."
 
 var convertedNumbers;
+var stringArray = [];
 
  $(function() {
    $("form").click(function(event) {
    event.preventDefault();
    string = ($("#input1").val());
    console.log(string);
-   for (var i = 0; i < #input.length; i ++)
-   if (string.includes 1) {
-
+   var stringArray = string.split("");
+   for (var i = 0; i < stringArray.length; i ++) {
+    if (stringArray[i].includes("3")) {
+      stringArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+      console.log(stringArray);
+    } else if (stringArray[i].includes("2")) {
+      stringArray[i] = "Boop!";
+    }
+      else if (stringArray[i].includes("1")) {
+      stringArray[i] = "Beep!";
    }
  });
 
