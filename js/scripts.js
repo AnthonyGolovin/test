5197c8da6fef3b9eb103011a44c6770be00dd55f
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-14 11:11:27 -0700

    Add .remove(); as a bandaid fix for text stacking for the timebeing, I google alot and couldnt figure it out so I am going to style and come back

--- js/scripts.js
+++ js/scripts.js
@@ -14,17 +14,19 @@ var stringArray = [];
 
    for (var i = 0; i < stringArray.length; i ++) {
     if (stringArray[i].includes("3")) {
-      stringArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
-
+      stringArray[i] = " ''I'm sorry, Dave. I'm afraid I can't do that.'' ";
+      $(this).remove();
     } else if (stringArray[i].includes("2") && !stringArray.includes("3")) {
-      stringArray[i] = "Boop!";
-
+      stringArray[i] = " ''Boop!'' ";
+      $(this).remove();
     } else if (stringArray[i].includes("1") && !string.includes("2") && !string.includes("3")) {
-      stringArray[i] = "Beep!";
+      stringArray[i] = " ''Beep!'' ";
+      $(this).remove();
     }
     }
     console.log(stringArray);
     $("#output").append(stringArray);
+
 
   });
 
